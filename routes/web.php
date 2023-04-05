be2b4f28bccc83dd1dd2fcee4ca30f33c0d9d123
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-04-05 04:27:48 +0300

    Listed quiz in database and created blade temp for quizzes list.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\QuizController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,4 +32,5 @@ Route::group([
     'middleware' => ['auth', 'isAdmin'],
     'prefix' => 'admin',
 ], function () {
+    Route::resource('quizzes', QuizController::class);
 });
