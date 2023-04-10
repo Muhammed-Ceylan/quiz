c1243a422869b096a79ff702fd669fd268f2328f
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-04-10 03:06:15 +0300

    Added mainController for quiz_detail.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\QuestionController;
 use App\Http\Controllers\Admin\QuizController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,14 +20,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('panel', [MainController::class, 'dashboard'])
+        ->name('dashboard');
+    Route::get('quiz/{slug}', [MainController::class, 'quiz_detail'])
+        ->name('quiz.detail');
 });
 
 Route::group([
